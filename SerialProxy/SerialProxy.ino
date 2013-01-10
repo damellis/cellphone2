5bf9b8fbbbbd4d377fca06a2511d4e29a9d6ebeb
David A. Mellis <d.mellis@arduino.cc>
2013-01-10 11:16:17 -0500

    Add serial proxy examples (for manual communication withthe GSM module).

---
+++ SerialProxy/SerialProxy.ino
@@ -0,0 +1,31 @@
+int pwrkey = 19;
+
+void setup() {
+  Serial.begin(9600);
+  
+  delay(1000);
+  digitalWrite(pwrkey, LOW);
+  pinMode(pwrkey, OUTPUT);
+  delay(2000);
+  pinMode(pwrkey, INPUT);
+  delay(1000);
+
+  Serial1.begin(9600);
+  Serial.println("READY");
+  Serial1.print("AT\r");
+}
+
+void loop() {
+  // read from port 1, send to port 0:
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte); 
+  }
+  
+  // read from port 0, echo to port 0, send to port 1:
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte); 
+    Serial.write(inByte);
+  }
+}
---
+++ SerialProxyGSM/SerialProxyGSM.ino
@@ -0,0 +1,38 @@
+#include <GSM3ShieldV1AccessProvider.h>
+#include <GSM3ShieldV1ModemCore.h>
+
+GSM3ShieldV1AccessProvider gsm;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Initializing GSM module...");
+  
+  // try to initialize the GSM module, retrying on failure
+  while (gsm.begin() != GSM_READY) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  Serial.println("done!");
+}
+
+void loop()
+{
+  char c;
+  boolean recv = false; // whether we got data from the GSM module
+  
+  // relay characters from the computer to the GSM module
+  while ((c = Serial.read()) != -1)
+    theGSM3ShieldV1ModemCore.write(c);
+    
+  // relay characters from the module to the computer
+  while ((c = theGSM3ShieldV1ModemCore.theBuffer().read()) != 0) {
+    Serial.write(c);
+    recv = true;
+  }
+  
+  // if the receive buffer filled up, the GSM library tells the module
+  // to stop sending us data. this tells it to start again.
+  if (recv) theGSM3ShieldV1ModemCore.gss.spaceAvailable();
+}
