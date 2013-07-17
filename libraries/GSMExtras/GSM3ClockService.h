a2c77c4f46556b51ad28b8aa1fb352efa1790ba1
David A. Mellis <d.mellis@arduino.cc>
2013-07-17 11:59:28 -0400

    Add MIT copyright notice and LGPL 2.1 to GSMExtras library.

--- libraries/GSMExtras/GSM3ClockService.h
+++ libraries/GSMExtras/GSM3ClockService.h
@@ -1,3 +1,24 @@
+/*
+ 
+(C) Copyright 2012-2013 Massachusetts Institute of Technology
+
+This file is part of the GSMExtras library.
+
+The GSMExtras library is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 2.1 of the
+License, or (at your option) any later version.
+
+The GSMExtras library is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GSMExtras library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef _GSM3CLOCKSERVICE_
 #define _GSM3CLOCKSERVICE_
 
